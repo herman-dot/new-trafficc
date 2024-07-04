215dc5f5f19401acf54b7e9a45ab140dd1abe729
herman-dot <herman.lindholm09@gmail.com>
2024-07-04 09:55:06 +0200

    Add files via upload

---
+++ trafficplus/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description '(RED)-ITA - Traffic Plus+ Lore-Friendly' 
+version '3.0_FiveM'
+
+files {
+    'tp/popgroups.ymt',
+    'tp/popcycle.dat'
+}
+
+data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'tp/popgroups.ymt'
+data_file 'POPSCHED_FILE' 'tp/popcycle.dat'
